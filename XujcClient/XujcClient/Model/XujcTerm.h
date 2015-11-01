08f4e99fa4676e661e5a0370ad298e51c404cc2b
luckytianyiyan <luckytianyiyan@gmail.com>
2015-11-01 22:20:08 +0800

    A Term Model

---
+++ XujcClient/XujcClient/Model/XujcTerm.h
@@ -0,0 +1,16 @@
+/**
+ * @file XujcTerm.h
+ *
+ * @author luckytianyiyan@gmail.com
+ * @date 15/11/1
+ * @copyright   Copyright © 2015年 luckytianyiyan. All rights reserved.
+ */
+
+#import "BaseModel.h"
+
+@interface XujcTerm : BaseModel
+
+@property(nonatomic, strong)NSString *termId;
+@property(nonatomic, strong)NSString *displayName;
+
+@end
---
+++ XujcClient/XujcClient/Model/XujcTerm.m
@@ -0,0 +1,13 @@
+/**
+ * @file XujcTerm.m
+ *
+ * @author luckytianyiyan@gmail.com
+ * @date 15/11/1
+ * @copyright   Copyright © 2015年 luckytianyiyan. All rights reserved.
+ */
+
+#import "XujcTerm.h"
+
+@implementation XujcTerm
+
+@end
