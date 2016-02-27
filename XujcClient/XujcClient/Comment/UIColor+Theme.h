38440b5be5f0223b2bb14a59eb0b812cbe482e03
luckytianyiyan <luckytianyiyan@gmail.com>
2016-02-27 09:50:54 +0800

    A UIColor+Theme

---
+++ XujcClient/XujcClient/Comment/UIColor+Theme.h
@@ -0,0 +1,18 @@
+//
+//  UIColor+Theme.h
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/2/27.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Theme)
+
++ (UIColor *)ty_textGreen;
++ (UIColor *)ty_textRed;
++ (UIColor *)ty_textBlack;
+
++ (UIColor *)ty_backgroundHighlight;
+@end
---
+++ XujcClient/XujcClient/Comment/UIColor+Theme.m
@@ -0,0 +1,33 @@
+//
+//  UIColor+Theme.m
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/2/27.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import "UIColor+Theme.h"
+
+@implementation UIColor (Theme)
+
++ (UIColor *)ty_textGreen
+{
+    return [UIColor colorWithHexString:@"#73c990"];
+}
+
++ (UIColor *)ty_textRed
+{
+    return [UIColor colorWithHexString:@"#ff6347"];
+}
+
++ (UIColor *)ty_textBlack
+{
+    return [UIColor colorWithHexString:@"#424243"];
+}
+
++ (UIColor *)ty_backgroundHighlight
+{
+    return [UIColor colorWithHexString:@"#f5f5f5"];
+}
+
+@end
