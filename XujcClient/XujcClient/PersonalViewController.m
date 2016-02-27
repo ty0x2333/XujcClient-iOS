9ed9669839b674c46d01a04162fa3265f8b33214
luckytianyiyan <luckytianyiyan@gmail.com>
2016-02-27 22:13:49 +0800

    M PersonalViewController, add settings button

--- XujcClient/XujcClient/PersonalViewController.m
+++ XujcClient/XujcClient/PersonalViewController.m
@@ -22,6 +22,11 @@
     self.title = NSLocalizedString(@"Personal", nil);
     _tableView = [[UITableView alloc] init];
     [self.view addSubview:_tableView];
+    
+    UIButton *settingsButton = [[UIButton alloc] initWithFrame:(CGRect){CGPointZero, CGSizeMake(25, 25)}];
+    [settingsButton setImage:[UIImage imageNamed:@"settings"] forState:UIControlStateNormal];
+    UIBarButtonItem *settingsButtonItem = [[UIBarButtonItem alloc] initWithCustomView:settingsButton];
+    self.navigationItem.rightBarButtonItem = settingsButtonItem;
 }
 
 - (void)viewDidLayoutSubviews
