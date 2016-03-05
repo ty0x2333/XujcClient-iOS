44995b3caf5f5b3c80edcb8389e3c3ddc7e93dd8
luckytianyiyan <luckytianyiyan@gmail.com>
2016-03-05 22:07:02 +0800

    A TYServerKeys

---
+++ XujcClient/XujcClient/API/TYServerKeys.h
@@ -0,0 +1,15 @@
+//
+//  TYServerKeys.h
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/3/5.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#ifndef TYServerKeys_h
+#define TYServerKeys_h
+
+static NSString* const TYServerKeyEmail = @"email";
+static NSString* const TYServerKeyPassword = @"password";
+
+#endif /* TYServerKeys.h */
