b2e2f10f3384dca2d036ca73830b2aee9fa504b6
luckytianyiyan <luckytianyiyan@gmail.com>
2016-03-06 00:39:12 +0800

    A AppConfig

---
+++ XujcClient/XujcClient/Comment/AppConfig.h
@@ -0,0 +1,14 @@
+//
+//  AppConfig.h
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/3/6.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#ifndef AppConfig_h
+#define AppConfig_h
+
+static CGFloat const kErrorHUDShowTime = 2.f;
+
+#endif /* AppConfig_h */
