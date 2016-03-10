d814dcee8754ee8e046937536f221e4dbad83a72
luckytianyiyan <luckytianyiyan@gmail.com>
2016-03-11 05:54:36 +0800

    A CourseEventViewModel

---
+++ XujcClient/XujcClient/ViewModel/CourseEventViewModel.h
@@ -0,0 +1,16 @@
+//
+//  CourseEventViewModel.h
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/3/11.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CourseEventViewModel : NSObject
+
+@property (copy, nonatomic) NSString *name;
+@property (copy, nonatomic) NSString *location;
+
+@end
