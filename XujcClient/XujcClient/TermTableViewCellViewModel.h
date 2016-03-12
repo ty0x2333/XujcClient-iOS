d293aacc82831318330b4836caa6d3fddde7400f
luckytianyiyan <luckytianyiyan@gmail.com>
2016-03-13 00:21:46 +0800

    A TermTableViewCellViewModel

---
+++ XujcClient/XujcClient/TermTableViewCellViewModel.h
@@ -0,0 +1,16 @@
+//
+//  TermTableViewCellViewModel.h
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/3/13.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "XujcTerm.h"
+
+@interface TermTableViewCellViewModel : NSObject
+
+@property (strong, nonatomic) XujcTerm *xujcTermModel;
+
+@end
---
+++ XujcClient/XujcClient/TermTableViewCellViewModel.m
@@ -0,0 +1,13 @@
+//
+//  TermTableViewCellViewModel.m
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/3/13.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import "TermTableViewCellViewModel.h"
+
+@implementation TermTableViewCellViewModel
+
+@end
