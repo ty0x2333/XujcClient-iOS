612870e2e7c449bbeaf5091c5bd8348ea0109d22
luckytianyiyan <luckytianyiyan@gmail.com>
2016-03-17 09:50:11 +0800

    A AppUtils

---
+++ XujcClient/XujcClient/Comment/AppUtils.h
@@ -0,0 +1,15 @@
+//
+//  AppUtils.h
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/3/17.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppUtils : NSObject
+
++ (UIViewController *)viewController:(UIView *)view;
+
+@end
---
+++ XujcClient/XujcClient/Comment/AppUtils.m
@@ -0,0 +1,24 @@
+//
+//  AppUtils.m
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/3/17.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import "AppUtils.h"
+
+@implementation AppUtils
+
++ (UIViewController *)viewController:(UIView *)view
+{
+    for (UIView *next = [view superview]; next; next = next.superview) {
+        UIResponder *nextResponder = [next nextResponder];
+        if ([nextResponder isKindOfClass:[UIViewController class]]) {
+            return (UIViewController *)nextResponder;
+        }
+    }
+    return nil;
+}
+
+@end
