86dbceb35a28ff0832bdb0526d81a4935ee473e1
luckytianyiyan <luckytianyiyan@gmail.com>
2016-03-18 08:31:41 +0800

    A SettingsViewModel

---
+++ XujcClient/XujcClient/SettingsViewModel.h
@@ -0,0 +1,15 @@
+//
+//  SettingsViewModel.h
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/3/18.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SettingsViewModel : NSObject
+
+@property (strong, nonatomic) RACCommand *executeLoginout;
+
+@end
