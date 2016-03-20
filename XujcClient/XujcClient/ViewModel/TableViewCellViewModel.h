eae9d6cd30b075532f95bff27974cd3c66dd05c6
luckytianyiyan <luckytianyiyan@gmail.com>
2016-03-21 01:45:09 +0800

    A TableViewCellViewModel

---
+++ XujcClient/XujcClient/ViewModel/TableViewCellViewModel.h
@@ -0,0 +1,19 @@
+//
+//  TableViewCellViewModel.h
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/3/20.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TableViewCellViewModel : NSObject
+
+@property (assign, nonatomic) UITableViewCellAccessoryType accessoryType;
+
+@property (copy, nonatomic) NSString *imageNamed;
+
+@property (copy, nonatomic) NSString *text;
+
+@end
---
+++ XujcClient/XujcClient/ViewModel/TableViewCellViewModel.m
@@ -0,0 +1,13 @@
+//
+//  TableViewCellViewModel.m
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/3/20.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import "TableViewCellViewModel.h"
+
+@implementation TableViewCellViewModel
+
+@end
