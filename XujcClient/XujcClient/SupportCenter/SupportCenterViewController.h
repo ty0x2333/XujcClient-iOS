3682612c52cbd388d3df4cf55a6713a8688c92d3
luckytianyiyan <luckytianyiyan@gmail.com>
2016-03-20 22:02:02 +0800

    A SupportCenterViewController

---
+++ XujcClient/XujcClient/SupportCenter/SupportCenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  SupportCenterViewController.h
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/3/20.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SupportCenterViewController : UITableViewController
+
+@end
---
+++ XujcClient/XujcClient/SupportCenter/SupportCenterViewController.m
@@ -0,0 +1,13 @@
+//
+//  SupportCenterViewController.m
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/3/20.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import "SupportCenterViewController.h"
+
+@implementation SupportCenterViewController
+
+@end
