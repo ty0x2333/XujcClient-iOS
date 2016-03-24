3f4e88a9c995f08fabc747c24690abd41eb7ceb7
luckytianyiyan <luckytianyiyan@gmail.com>
2016-03-24 23:38:56 +0800

    A UserDetailViewController

---
+++ XujcClient/XujcClient/UserDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserDetailViewController.h
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/3/24.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserDetailViewController : UIViewController
+
+@end
---
+++ XujcClient/XujcClient/UserDetailViewController.m
@@ -0,0 +1,19 @@
+//
+//  UserDetailViewController.m
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/3/24.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import "UserDetailViewController.h"
+
+@implementation UserDetailViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.title = NSLocalizedString(@"Personal Detail", nil);
+}
+
+@end
