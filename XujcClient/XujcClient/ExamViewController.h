42da95124fe6d57e4d12827b96336dcaad58d363
luckytianyiyan <luckytianyiyan@gmail.com>
2016-04-18 22:08:11 +0800

    A ExamViewController

---
+++ XujcClient/XujcClient/ExamViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExamViewController.h
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/4/18.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ExamViewController : BaseViewController
+
+@end
---
+++ XujcClient/XujcClient/ExamViewController.m
@@ -0,0 +1,37 @@
+//
+//  ExamViewController.m
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/4/18.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import "ExamViewController.h"
+
+@interface ExamViewController ()
+
+@end
+
+@implementation ExamViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.screenName = @"Exam";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
