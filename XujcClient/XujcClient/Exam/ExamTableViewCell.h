a186f527190e0ba55cbf24de88255d7f107312aa
luckytianyiyan <luckytianyiyan@gmail.com>
2016-04-18 23:37:48 +0800

    A ExamTableViewCell

---
+++ XujcClient/XujcClient/Exam/ExamTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ExamTableViewCell.h
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/4/18.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import "InformationTableViewCell.h"
+
+@interface ExamTableViewCell : InformationTableViewCell
+
+@end
---
+++ XujcClient/XujcClient/Exam/ExamTableViewCell.m
@@ -0,0 +1,21 @@
+//
+//  ExamTableViewCell.m
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/4/18.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import "ExamTableViewCell.h"
+
+@implementation ExamTableViewCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
