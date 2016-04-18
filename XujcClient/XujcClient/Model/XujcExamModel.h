b4d3c87ae8ba9e53a882a05d2a212080fd1e35cb
luckytianyiyan <luckytianyiyan@gmail.com>
2016-04-18 22:53:34 +0800

    A XujcExamModel

---
+++ XujcClient/XujcClient/Model/XujcExamModel.h
@@ -0,0 +1,24 @@
+//
+//  XujcExamModel.h
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/4/18.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import "BaseModel.h"
+
+@interface XujcExamModel : BaseModel
+
+@property (nonatomic, copy) NSString* lessonName;
+@property (nonatomic, copy) NSString* location;
+
+@property (nonatomic, copy) NSString* startData;
+@property (nonatomic, copy) NSString* timePeriod;
+@property (nonatomic, copy) NSString* time;
+@property (nonatomic, copy) NSString* way;
+@property (nonatomic, copy) NSString* week;
+@property (nonatomic, copy) NSString* name;
+@property (nonatomic, copy) NSString* status;
+
+@end
