c6e3902a4eba95ec98fd6d4df3e26ab87f0b47f4
luckytianyiyan <luckytianyiyan@gmail.com>
2016-04-20 15:34:23 +0800

    A LessonEventPopViewModel

---
+++ XujcClient/XujcClient/Schedule/LessonEventPopViewModel.h
@@ -0,0 +1,17 @@
+//
+//  LessonEventPopViewModel.h
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/4/20.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import "LessonEventViewModel.h"
+
+@interface LessonEventPopViewModel : LessonEventViewModel
+
+@property (nonatomic, readonly, copy) NSString *weekInterval;
+
+@property (nonatomic, readonly, copy) NSString *startEndWeek;
+
+@end
