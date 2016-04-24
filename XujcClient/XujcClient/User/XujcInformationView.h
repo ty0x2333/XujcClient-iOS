d0e01a4a2dc637b95dfe88bf8bd491a6c692b265
luckytianyiyan <luckytianyiyan@gmail.com>
2016-04-24 21:54:02 +0800

    A XujcInformationView

---
+++ XujcClient/XujcClient/User/XujcInformationView.h
@@ -0,0 +1,13 @@
+//
+//  XujcInformationView.h
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/4/24.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XujcInformationView : UIView
+
+@end
---
+++ XujcClient/XujcClient/User/XujcInformationView.m
@@ -0,0 +1,22 @@
+//
+//  XujcInformationView.m
+//  XujcClient
+//
+//  Created by 田奕焰 on 16/4/24.
+//  Copyright © 2016年 luckytianyiyan. All rights reserved.
+//
+
+#import "XujcInformationView.h"
+
+@interface XujcInformationView()
+
+@property (strong, nonatomic) UILabel *studentIdLabel;
+@property (strong, nonatomic) UILabel *nameLabel;
+@property (strong, nonatomic) UILabel *gradeLabel;
+@property (strong, nonatomic) UILabel *professionalLabel;
+
+@end
+
+@implementation XujcInformationView
+
+@end
