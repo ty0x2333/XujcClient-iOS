d86261ebecb9f192c9d8b5f7d166d7673f6cbc27
luckytianyiyan <luckytianyiyan@gmail.com>
2016-05-02 17:40:13 +0800

    M ScheduleViewModel rename p_coureEventsFromDayNumberOfWeek:dayNumberOfWeek: to p_coureEvents:chineseDayOfWeek:

--- XujcClient/XujcClient/Schedule/ScheduleViewModel.m
+++ XujcClient/XujcClient/Schedule/ScheduleViewModel.m
@@ -66,17 +66,16 @@
 {
     NSMutableArray *events = [[NSMutableArray alloc] initWithCapacity:kDayCountOfWeek];
     for (NSInteger i = 0; i < kDayCountOfWeek; ++i) {
-        [events addObject:[self p_coureEventsFromDayNumberOfWeek:lessonEvents dayNumberOfWeek:i + 1]];
+        [events addObject:[self p_coureEvents:lessonEvents chineseDayOfWeek:i + 1]];
     }
     return [events copy];
 }
 
-- (NSArray *)p_coureEventsFromDayNumberOfWeek:(NSArray *)allLessonEvents dayNumberOfWeek:(NSInteger)dayNumberOfWeek
+- (NSArray *)p_coureEvents:(NSArray *)allLessonEvents chineseDayOfWeek:(NSInteger)chineseDayOfWeek
 {
     NSMutableArray *result = [[NSMutableArray alloc] init];
     for (XujcLessonEventModel *event in allLessonEvents) {
-        NSInteger currentDayNumberOfWeek = [event chineseDayOfWeek];
-        if (currentDayNumberOfWeek == dayNumberOfWeek){
+        if ([event chineseDayOfWeek] == chineseDayOfWeek){
             [result addObject:event];
         }
     }
