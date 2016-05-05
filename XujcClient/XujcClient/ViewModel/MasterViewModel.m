8e8d19c97c82dc691f12cc1028b5115f0e313255
luckytianyiyan <luckytianyiyan@gmail.com>
2016-05-05 19:26:39 +0800

    M UMeng auto set Alias

--- XujcClient/XujcClient/ViewModel/MasterViewModel.m
+++ XujcClient/XujcClient/ViewModel/MasterViewModel.m
@@ -10,6 +10,7 @@
 #import "DynamicData.h"
 #import "UserModel.h"
 #import <Instabug/Instabug.h>
+#import "UMessage.h"
 
 @implementation MasterViewModel
 
@@ -17,18 +18,43 @@
 {
     if (self = [super init]) {
         NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-        RACSignal *shakingReportStatusSignal = [[[userDefaults ty_channelTerminalForShakingReportStatus] setNameWithFormat:@"MasterViewModel shakingReportStatusChannel"] logAll];
+        RACSignal *shakingReportStatusSignal = [[[userDefaults ty_channelTerminalForShakingReportStatus] setNameWithFormat:@"MasterViewModel shakingReportStatusChannel"] ty_logNext];
         [shakingReportStatusSignal subscribeNext:^(NSNumber *value) {
             [Instabug setInvocationEvent:[value boolValue] ? IBGInvocationEventShake : IBGInvocationEventNone];
         }];
         
-        RACSignal *userSignal = [[[[userDefaults ty_channelTerminalForUser] map:^id(NSData *value) {
+        RACSignal *userSignal = [[[[[userDefaults ty_channelTerminalForUser] ignore:nil] map:^id(NSData *value) {
             UserModel *user = [NSKeyedUnarchiver unarchiveObjectWithData:value];
             return user;
-        }] setNameWithFormat:@"MasterViewModel userSignal"] logAll];
-
+        }] setNameWithFormat:@"MasterViewModel userSignal"] ty_logNext];
+        
         [userSignal subscribeNext:^(UserModel *model) {
-            [Instabug setUserEmail:model.email];
+//            [Instabug setUserEmail:model.email];
+            [Instabug setUserName:model.nikename];
+        }];
+        
+        RACSignal *deviceTokenSignal = RACObserve(self, deviceToken);
+        
+        RACSignal *deviceTokenActiveSignal = [deviceTokenSignal ignore:nil];
+        
+        [deviceTokenSignal subscribeNext:^(NSData *data) {
+            [UMessage registerDeviceToken:data];
+        }];
+        
+        RACSignal *userPhoneSignal = [[[userSignal map:^id(UserModel *model) {
+            return model.phone;
+        }] filter:^BOOL(id value) {
+            return [value isKindOfClass:[NSString class]] && (value != nil);
+        }] distinctUntilChanged];
+        
+        [[RACSignal combineLatest:@[deviceTokenActiveSignal, userPhoneSignal] reduce:^id(NSData *data, NSString *phone){
+            return phone;
+        }] subscribeNext:^(NSString *phone) {
+            [UMessage setAlias:phone type:@"TYService" response:^(id responseObject, NSError *error) {
+                if (error) {
+                    TyLogFatal(@"%@", error);
+                }
+            }];
         }];
     }
     return self;
